cc874ba2c488ae5b6d84973a759b2632c1cefa92
SalomeReav <salome.reav.50@gmail.com>
2023-05-11 18:31:43 +0200

    prueba lectura de un json de declaración de variable, pinta por pantalla el xml de un lugar

--- Classes/main.py
+++ Classes/main.py
@@ -1,15 +1,37 @@
-from OutputPnmlClass import OutputPNML
+from OutputPnml import OutputPNML
+from PetriNet import PetriNet 
+from Place import Place  
+from Transition import Transition 
+from Arc import Arc 
 import json
 
 #node types of interest
-#ECL_TYPES = ["VarDecl"]
+DECL_TYPES = ["VarDecl"]
+CONTROL_TYPES = ["IfStmt"]
+CALL_TYPES = ["CallExpr"]
+TYPES_OF_INTEREST = DECL_TYPES + CONTROL_TYPES + CALL_TYPES 
 
+if __name__== "__main__":
 
-#f __name__== "__main__":
+    paint = OutputPNML()
 
-j = open('curated_ast.json')
-myjson = json.load(j)
-print(myjson)
-    
+    j = open('curated_ast.json')
+    myjson = json.load(j)
+
+    for key in myjson:
+        for value in myjson[key]:
+            for type in myjson[key][value]:
+                if "kind" in type:
+                    if  myjson[key][value][type] in DECL_TYPES:
+                        nameValue= myjson[key][value][type]
+                        Id= myjson[key][value]["id"]
+                        #cambiar
+                        principalNet = PetriNet()
+                        placeAux = Place(nameValue,Id)
+                        principalNet.nodes = placeAux 
+                        paint.writePlace(principalNet.nodes)
+
+                   
+
 
     #aux = prueba.createOutputTransition()
